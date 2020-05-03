fcc0dd2fd6e54e463778c79368c5f7ff69a98c05
nayeem <ctgnayeem0@gmail.com>
2020-05-04 02:36:36 +0600

    added function

--- hello.js
+++ hello.js
@@ -1,6 +1,14 @@
 console.log('Hello world')
 
-let a = 2 
-let b = 3 
+let a = 2
+let b = 3
 
-console.log(a+b)
+console.log(a + b)
+
+
+let a = 10
+console.log(a * a)
+
+function () {
+    return a + b
+}
